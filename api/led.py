9cadfee2d85f240cdba530c7329d4dad406523c6
Brian Silverman <bri@blueacresembedded.com>
2013-10-02 12:32:51 -0400

    Boucning ball test

--- api/led.py
+++ api/led.py
@@ -30,14 +30,20 @@ def erase(color=0):
     global fb
     fb = [[color]*8 for i in range(8)]
 
-def point(x, y, color=1):
+def point(x, y=None, color=1):
+    # If y is not given, then x is a tuple of the point
+    if y == None:
+        x, y = x
+
     # If out of range, its off the screen - just don't display it
     try:
-        fb[x][y] = color;
+        fb[int(x)][int(y)] = color;
     except IndexError:
         pass
 
-def line(point_a, point_b):
+def line(point_a, point_b, color=1):
+    point_a = (int(point_a[0]), int(point_a[1]))
+    point_b = (int(point_b[0]), int(point_b[1]))
     x_diff = point_a[0] - point_b[0]
     y_diff = point_a[1] - point_b[1]
     step = sign(x_diff) * sign(y_diff)
@@ -45,20 +51,26 @@ def line(point_a, point_b):
     height = abs(y_diff) + 1
     if (width > height):
         start_point = point_a if x_diff < 0 else point_b
-        for x_offset in range(width):
-            point(start_point[0] + x_offset, start_point[1] + step*(x_offset*height/width))
+        for x_offset in range(int(width)):
+            point(
+                start_point[0] + x_offset,
+                start_point[1] + step*(x_offset*height/width),
+                color=color)
     else:
         start_point = point_a if y_diff < 0 else point_b
-        for y_offset in range(height):
-            point(start_point[0] + step*(y_offset*width/height), start_point[1] + y_offset)
+        for y_offset in range(int(height)):
+            point(
+                start_point[0] + step*(y_offset*width/height),
+                start_point[1] + y_offset,
+                color=color)
     
-def rect(start, dimensions):
+def rect(start, dimensions, color=1):
     x, y = start
     width, height = dimensions
-    line((x, y), (x, y + height))
-    line((x, y + height), (x + width, y + height))
-    line((x + width, y + height), (x + width, y))
-    line((x + width, y), (x, y))
+    line((x, y), (x, y + height), color=color)
+    line((x, y + height), (x + width, y + height), color=color)
+    line((x + width, y + height), (x + width, y), color=color)
+    line((x + width, y), (x, y), color=color)
 
 def show():
     s = ""
--- apps/test.py
+++ apps/test.py
@@ -1,5 +1,6 @@
 import time
 import led
+import math
 
 """
 for i in range(1):
@@ -21,6 +22,7 @@ for x1 in range(8):
                 time.sleep(0.005);
                 led.erase()
 """
+"""
 for x1 in range(8):
     for y1 in range(8):
         for x2 in range(8):
@@ -29,3 +31,35 @@ for x1 in range(8):
                 led.show()
                 time.sleep(0.05);
                 led.erase()
+"""
+def new_vector(ball, direction, speed, time, width=8.0, height=8.0):
+    x, y = ball
+    distance = speed * time
+    x = x + distance * math.cos(math.radians(direction))
+    y = y + distance * math.sin(math.radians(direction))
+    if x >= width:
+        x = 2 * width - x
+        direction = 180 - direction
+    if x < 0:
+        x = -x
+        direction = 180 - direction
+    if y >= height:
+        y = 2 * height - y
+        direction = -direction
+    if y < 0:
+        y = - y
+        direction = -direction
+    return ((x, y), direction)
+    
+ball = (0.0, 0.0)
+direction = 70.0
+speed = 100.0
+period = 0.01
+dimensions = (1,1)
+while True:
+    led.rect(ball, dimensions)
+    led.show()
+    time.sleep(period);
+    led.rect(ball, dimensions, color=0)
+    ball, direction = new_vector(ball, direction, speed, period, width=7, height=7)
+
