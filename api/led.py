647179a9ae16a74b04849c361ad5f0888da4c83d
Brian Silverman <bri@blueacresembedded.com>
2013-10-02 05:29:59 -0400

    Added initial point and show api/led functions

--- api/led.py
+++ api/led.py
@@ -4,23 +4,43 @@ import sys
 import fcntl
 from subprocess import *
 
-pipe = 0
-
 def _send_cmd(cmd, data):
-    global pipe
     pipe.write(cmd + ("%02X" % len(data)) + data)
 
 def _init_module():
+    erase()
     global pipe
     pipe = Popen("api/led_server", shell=True, stdin=PIPE).stdin
     _send_cmd("n", "1")
     _send_cmd("o", "43581267" + "65218743")
 
-_init_module()
-if __name__ == "__main__":
+def _main():
     while 1:
         for i in range(8):
             for j in range(8):
-                s = ("00" * i) + ("%02X" % (1<<j)) + ("00" * (7-i))
-                _send_cmd("m", s);
-                time.sleep(0.05);
+                led.point(x, y)
+                led.show()
+                time.sleep(0.5);
+                led.point(x, y, color=0)
+
+def erase(color=0):
+    global fb
+    fb = [[color]*8 for i in range(8)]
+
+def point(x, y, color=1):
+    fb[x][y] = color;
+
+def show():
+    s = ""
+    for x in range(8):
+        col = 0
+        for y in range(8):
+            col |= fb[x][y] << (8-y-1)
+        s += "%02X" % col
+    _send_cmd("m", s);
+
+
+_init_module()
+if __name__ == "__main__":
+    _main()
+
--- apps/test.py
+++ apps/test.py
@@ -2,8 +2,9 @@ import time
 import led
 
 while 1:
-    for i in range(8):
-        for j in range(8):
-            s = ("00" * i) + ("%02X" % (1<<j)) + ("00" * (7-i))
-            led._send_cmd("m", s);
-            time.sleep(0.05);
+    for x in range(8):
+        for y in range(8):
+            led.point(x, y)
+            led.show()
+            time.sleep(0.1);
+            led.point(x, y, color=0)
