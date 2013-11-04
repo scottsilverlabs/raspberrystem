be19d728877e651e0485b8d857d6ad1171427bbf
Brian Silverman <bri@blueacresembedded.com>
2013-11-03 19:21:58 -0500

    gpio: Working demo app with two moveable dots

--- api/gpio.py
+++ api/gpio.py
@@ -5,21 +5,22 @@ from threading import Thread, Lock
 from collections import namedtuple
 import types
 
-GPIOS=[2,3,4,14,15,17,18,22,23,24,25,27]
-POLL_RATE=0.010
+PINS=[2,3,4,14,15,17,18,22,23,24,25,27]
 OUTPUT=1
 INPUT=2
 DISABLED=3
 
 class Gpio:
-    def __init__(self, gpio):
-        self.gpio_dir = "/sys/class/gpio/gpio%d" % gpio
-        self.gpio = gpio
+    def __init__(self, pin):
+        self.gpio_dir = "/sys/class/gpio/gpio%d" % pin
+        self.pin = pin
         self.direction = DISABLED
-        if gpio in GPIOS:
+        self.last = 1
+        self.mutex = Lock()
+        if pin in PINS:
             if not os.path.exists(self.gpio_dir):
                 with open("/sys/class/gpio/export", "w") as f:
-                    f.write("%d\n" % gpio)
+                    f.write("%d\n" % pin)
 
     def configure(self, direction):
         if direction == OUTPUT:
@@ -31,71 +32,68 @@ class Gpio:
         else:
             raise ValueError("Direction must be INPUT, OUTPUT or DISABLED")
 
-        with open(self.gpio_dir + "/direction", "w") as fdirection:
-            self.direction = direction
-            if direction == OUTPUT:
-                # For future use
-                fdirection.write("out")
-                self.fvalue = open(self.gpio_dir + "/value", "w")
-            elif direction == INPUT:
-                fdirection.write("in")
-                self.fvalue = open(self.gpio_dir + "/value", "r")
-            elif direction == DISABLED:
-                fdirection.write("in")
-                self.fvalue.close()
+        with self.mutex:
+            with open(self.gpio_dir + "/direction", "w") as fdirection:
+                self.direction = direction
+                if direction == OUTPUT:
+                    # For future use
+                    fdirection.write("out")
+                    self.fvalue = open(self.gpio_dir + "/value", "w")
+                elif direction == INPUT:
+                    fdirection.write("in")
+                    self.fvalue = open(self.gpio_dir + "/value", "r")
+                elif direction == DISABLED:
+                    fdirection.write("in")
+                    self.fvalue.close()
+
+    def was_clicked(self):
+        level = self.get_level()
+        clicked = level == 1 and self.last == 0
+        self.last = level
+        return clicked
 
     def get_level(self):
-        self.fvalue.seek(0)
-        return int(self.fvalue.read())
+        with self.mutex:
+            self.fvalue.seek(0)
+            return int(self.fvalue.read())
 
     def set_level(self, level):
-        raise NotImplementedError()
+        with self.mutex:
+            raise NotImplementedError()
 
 
 class Gpios:
     def __init__(self):
-        self.gpios = [Gpio(i) for i in range(max(GPIOS) + 1)]
-        self.mutex = Lock()
-        t = Thread(target=self.__gpio_polling_thread)
-        t.daemon = True
-        t.start()
+        self.gpios = [Gpio(i) for i in range(max(PINS) + 1)]
 
-    def __validate_gpio(self, gpio, direction):
+    def __validate_gpio(self, pin, direction):
         class UninitializedError(Exception):
             pass
-        if not gpio in GPIOS:
+        if not pin in PINS:
             raise ValueError("Invalid GPIO")
-        if self.gpios[gpio].direction != direction:
+        if self.gpios[pin].direction != direction:
             raise UninitializedError()
 
-    def __gpio_polling_thread(self):
-        while True:
-            with self.mutex:
-                pass
-            time.sleep(POLL_RATE)
-
-    def configure(self, gpio, direction):
-        print self, gpio, direction
-        if not gpio in GPIOS:
+    def configure(self, pin, direction):
+        if not pin in PINS:
             raise ValueError("Invalid GPIO")
-        with self.mutex:
-            self.gpios[gpio].configure(direction)
+        self.gpios[pin].configure(direction)
                 
-    def get_presses(self, gpio):
-        self.__validate_gpio(gpio, INPUT)
-        raise NotImplementedError()
+    def was_clicked(self):
+        inputs = [g for g in self.gpios if g.direction == INPUT]
+        return [g.pin for g in inputs if g.was_clicked()]
 
-    def get_level(self, gpio):
-        self.__validate_gpio(gpio, INPUT)
-        return self.gpios[gpio].get_level()
+    def get_level(self, pin):
+        self.__validate_gpio(pin, INPUT)
+        return self.gpios[pin].get_level()
 
-    def set_level(self, gpio, level):
-        self.__validate_gpio(gpio, OUTPUT)
-        self.gpios[gpio].set_level(level)
+    def set_level(self, pin, level):
+        self.__validate_gpio(pin, OUTPUT)
+        self.gpios[pin].set_level(level)
 
 # Export functions in this module
 g = Gpios()
-for name in ['configure', 'get_presses', 'get_level', 'set_level']:
+for name in ['configure', 'get_level', 'set_level', 'was_clicked']:
     globals()[name] = getattr(g, name)
 
 def _main():
