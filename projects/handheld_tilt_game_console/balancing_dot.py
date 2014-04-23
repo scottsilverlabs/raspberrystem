a3c714cde62e3d8d7929ac432e037a7fd9046734
Brian Silverman <bri@blueacresembedded.com>
2014-04-23 17:59:48 -0400

    accel: balancing_dot: Fixed broken call
    Also made start position dynamic based on center of LED

--- projects/handheld_tilt_game_console/balancing_dot.py
+++ projects/handheld_tilt_game_console/balancing_dot.py
@@ -3,12 +3,12 @@ from rstem import led
 from rstem import accel
 
 xbase, ybase =  accel.get_data()
-x, y = (4, 4)
+x, y = (led.width()/2, led.height()/2)
 while True:
     xaccel, yaccel =  accel.get_data()
     xchg = (xaccel - xbase)/20.0
     ychg = (yaccel - ybase)/20.0
-    x, y = led.bound(x + xchg, y + ychg)
+    x, y = led.bound(int(x + xchg), int(y + ychg))
 
     led.erase()
     led.point(x, y)
