140ffeda2a3c49271696b9900b0fd7b215fd79d4
Jon Snyder <jonsnyder1@gmail.com>
2014-06-09 14:29:11 -0400

    add relocated MANIFEST.in, setup.py as well as debian files

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "RaspberrySTEM",
+    version = "0.0.1",
+    author = "Brian Silverman",
+    author_email = "bri@raspberrystem.com",
+    description = ("RaspberrySTEM Educational and Hobbyist Development Kit "
+                    "based on the Raspberry Pi."),
+    license = "BSD",
+    keywords = "raspberry pi",
+    url = "https://github.com/scottsilverlabs/raspberrystem",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    # use https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Education",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=[
+        'bitstring',
+    ],
+)
+
+# TODO: register python package to https://pypi.python.org/pypi
