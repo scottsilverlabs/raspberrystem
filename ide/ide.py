705d5860bdc575d969a3e9134d3aed929f2e08f0
Stephan Wolski <me@stephanwolski.com>
2014-06-13 16:55:42 -0400

    Changed grid

--- ide/ide.py
+++ ide/ide.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) 2014, Scott Silver Labs, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #Depends on python3-gi, gir1.2-gtksource-3.0, and gir1.2-webkit-3.0
-#TODO Copyright/License
-#TODO Buttons
 
 from gi.repository import Gtk, GtkSource, WebKit
+
 class IDE(Gtk.Window):
-    lastpage = "dev.raspberrystem.com/wphidden42/"
+    
     def __init__(self):
         Gtk.Window.__init__(self, title="Raspberry IDEa")
         self.set_size_request(400, 400)
