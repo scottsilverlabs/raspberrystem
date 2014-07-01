310a8915b49c3e02964dc3310da0cfb4a20a484b
JoeWolski <wolski.joseph@gmail.com>
2014-07-01 15:50:23 +0000

    Added breif comments

--- rstem/blink/BlinkLED.py
+++ rstem/blink/BlinkLED.py
@@ -1,14 +1,18 @@
-#!/usr/bin/env python3
+#Basic imports
 import RPi.GPIO as GPIO
 import time
 
+#Declare pins and setup
 led = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
 
+#Blink 10 times
 for x in range(0,10):
 	GPIO.output(led, True)
 	time.sleep(1)
 	GPIO.output(led, False)
 	time.sleep(1)
+
+#cleanup
 GPIO.cleanup()
