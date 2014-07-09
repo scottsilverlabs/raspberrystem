45dcb2370282efd9fe47718e10170316c8293877
Jon Snyder <jonsnyder1@gmail.com>
2014-07-09 18:17:35 -0400

    finished basic implementation of game, time to debug...

--- projects/handheld_game_console/stackem.py
+++ projects/handheld_game_console/stackem.py
@@ -21,12 +21,15 @@ import RPi.GPIO as GPIO
 
 class State:
     WIN, LOSE, PLAYING, IDLE, DONE = range(5)
+class Direction:
+    LEFT, RIGHT, UP, DOWN = range(4)
     
 class Block(object):
     def __init__(self, origin, width, color=0xF):
         self.origin = origin
         self.width = width
         self.color = color
+        self.direction = Direction.RIGHT  # start moving to right
         
     def draw(self):
         """Draws block on led matrix display"""
@@ -37,24 +40,67 @@ class Block(object):
     def stop(self):
         """Stops the block from moving"""
         pass
+        
+    def adjacent(self, other):
+        """Returns true if self and other are touching"""
+        pass
+        
+    def match(self, other):
+        """Changes block to be the same width x offset of other block"""
+        self.origin = (other.origin[0], self.origin[1])  # set x param to be same
+        self.width = other.width
+        
+    def move(self, direction):
+        """Moves block on step in given direction"""
+        if direction == Direction.LEFT:
+            self.origin = (self.origin[0]-1, self.origin[1])
+        elif direction == Direction.RIGHT:
+            self.origin = (self.origin[0]+1, self.origin[1])
+        elif direction == Direction.UP:
+            self.origin = (self.origin[0], self.origin[1]+1)
+        elif direction == Direction.DOWN:
+            self.origin = (self.origin[0], self.origin[1]-1)
+        else:
+            raise RuntimeError("Invalid Direction")
 
 try:
     # SETUP ==========================
-
-    # initialize variables
-    curr_state = State.IDLE  # current state used for state machine
-    blocks = []              # current block elements on screen
 
     # setup gpio buttons
     BUTTON=2  # GPIO pin button is connected to
     def button_handler(channel):
         if channel == BUTTON:
             if curr_state == State.PLAYING:
-                blocks[-1].stop()  # stop the current moving block
-                if blocks[-2]
+                # stop the block
+                if len(blocks) > 1:
+                    if blocks[-1].adjacent(blocks[-2]):
+                        blocks[-1].match(blocks[-2])  # make block same size and position
+                    else:
+                        curr_state = State.LOSE
+                        return
+                
+                # show stopped block for a couple seconds
+                draw_blocks()
+                led2.show()
+                time.sleep(1)
+                
+                # if hit the ceiling, display win screen 
+                if len(blocks) == HEIGHT:
+                    curr_state = State.WIN
+                    return
+                    
+                # create new block
+                if len(blocks) % change_level == 0:  # check if to up difficulty
+                    if curr_width > 1:
+                        curr_width -= 1
+                    curr_speed += 1  # update current speed
+                curr_width = min(blocks[-1].width, curr_width)
+                blocks.append(Block((0, blocks[-1].origin[1]+1), curr_width))
+                
             elif curr_state in [State.IDLE, State.WIN, State.LOSE]:
                 # set up game
-                blocks = []  # reset blocks
+                blocks = [Block((0,0), start_width, start_speed)]  # set up first block
+                led2.erase()
                 draw_blocks()
                 led2.show()
                 curr_state = State.PLAYING
@@ -66,10 +112,20 @@ try:
     led2.init_grid(2,1,180)
     HEIGHT = led2.height()
     WIDTH = led2.width()
+    
+    # initialize variables
+    curr_state = State.IDLE  # current state used for state machine
+    blocks = []              # current block elements on screen
+    start_width = 3           # pixel width of block on start
+    start_speed = 2           # current speed on start (in pixel/sec)
+    start_level = 1           # level of difficulty to start with 
+    change_level = int(HEIGHT*(1/3.))  # number of blocks before upping difficulty
+    curr_width, curr_speed = start_width, start_speed
+    curr_direction = Direction.RIGHT  # current direction top block is moving
 
+    # helper functions
     def draw_blocks():
         """Draws the current blocks on screen"""
-        led2.erase()
         for block in blocks:
             block.draw()
             
@@ -77,18 +133,53 @@ try:
     
     while curr_state != State.DONE:
         if curr_state == State.IDLE:
-            # TODO: do something cool
-            pass
+            # TODO: do something cooler
+            led2.point(0,0)
+            led2.point(WIDTH-1,0)
+            led2.point(0,HEIGHT-1)
+            led2.point(WIDTH-1, HEIGHT-1)
+            led2.show()
+
         elif curr_state == State.PLAYING:
-            pass
+            # change direction if hit edge of screen
+            if curr_direction == Direction.RIGHT:
+                if blocks[-1].origin[0] + blocks.width == WIDTH-1:
+                    curr_direction = Direction.LEFT
+            elif blocks[-1].origin[0] == 0:
+                curr_direction = Direction.RIGHT
+                
+            # move top block in curr_direction at curr_speed
+            blocks[-1].move(curr_direction)
+            led2.erase()
+            draw_blocks()
+            led2.show()
+            time.sleep(1/curr_speed)
+            
         elif curr_state == State.WIN:
-            pass
+            led2.erase()
+            led2.text("WIN!", font_size="small")
+            led2.show()
+            time.sleep(3)
+
         elif curr_state == State.LOSE:
-            pass
+            # blink failing block
+            blocks[-1].color = 0
+            led2.erase()
+            draw_blocks()
+            led2.show()
+            time.sleep(1)
+            led2.erase()
+            blocks[-1].color = 0xF
+            draw_blocks()
+            led2.show()
+            time.sleep(1)
+
         else:
             raise RuntimeError("Invalid State")
         
-    
+finally:
+    led2.shutdown_matrices()
+    GPIO.cleanup()
     
     
     
