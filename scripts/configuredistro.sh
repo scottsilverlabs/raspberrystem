f7dbf737319fbc8b85cfd75557ac913322cc7523
Stephan Wolski <steff.wolski@gmail.com>
2014-07-10 15:34:19 -0400

    Added more services for removal

--- scripts/configuredistro.sh
+++ scripts/configuredistro.sh
@@ -1,11 +1,7 @@
-sudo apt-get -y autoremove wolfram-engine dillo lxsession lxterminal lightdm scratch sonic-pi rsync #Remove uneeded programs 
+sudo apt-get -y autoremove wolfram-engine dillo lxsession lxterminal lightdm scratch sonic-pi rsync cron triggerhappy #Remove uneeded programs 
 sudo apt-get -y install chromium
 sudo sed -i "s/exit 0\/sudo \/usr\/bin\/ideserver &\nnetpd -gqx &\nexit 0/" /etc/rc.local #Start IDE server and set time on boot
 sudo update-rc.d -f ntp disable #Disable the time daemon, it's pointless on the Pi
-sudo update-rc.d -f lightdm disable #Lightdm was removed, but the service remains
-sudo update-rc.d -f plymouth disable #Plymouth is uneeded
-sudo update-rc.d -f cron disable #Cron is uneeded
-sudo update-rc.d -f rsync disable #Rsync is uneeded
 sudo sed -i "s/1:2345:respawn:\/sbin\/getty --noclear 38400 tty1/1:2345:respawn:\/bin\/login -f root tty1 <\/dev\/tty1 >\/dev\/tty1 2>&1/" #Have TTY1 auto login to root
 sudo echo '#!/bin/bash
 if [[ -z $DISPLAY && $(tty) = /dev/tty1 ]]; then
@@ -21,7 +17,6 @@ sudo cat /dev/null > /etc/X11/default-display-manager #default-display-manager i
 sed -i "/^overscan_.*/d" /boot/config.txt
 sed -i "s/^#disable_overscan/disable_overscan/" /boot/config.txt
 sed -i "s/^gpu_mem=.*/gpu_mem=16/" /boot/config.txt
-set_config_var
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 sudo locale-gen
 sudo dpkg-reconfigure keyboard-configuration
