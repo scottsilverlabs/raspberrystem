63e5d77776fc7a85e65a2b4c31eabc33b609f62e
Jon Snyder <jonsnyder1@gmail.com>
2014-07-10 12:11:44 -0400

    debuging game, need to fix syncing issues

--- projects/handheld_game_console/stackem.py
+++ projects/handheld_game_console/stackem.py
@@ -43,38 +43,85 @@ class Block(object):
         
     def adjacent(self, other):
         """Returns true if self and other are touching"""
-        pass
+        if (self.origin[0] > (other.origin[0]+other.width))  \
+            or ((self.origin[0]+self.width-1) < other.origin[0]):
+            print("False")
+            return False
+        else:
+            print("True")
+            return True
+                    
+    def crop(self, other):
+        """Returns a block to be the same width x offset of other block"""
+        offset = self.origin[0] - other.origin[0]
+        loffset = (self.origin[0]+self.width) - (other.origin[0]+other.width)
+        if offset >= 0:  # crop rightmost
+            width = self.width - loffset
+            origin = self.origin
+        elif offset < 0:  # crop left most
+            width = self.width + offset  # chop off offset amount
+            origin = [other.origin[0], self.origin[1]]
+#            self.origin[0] = other.origin[0] # move to right
+        return Block(origin, width)
+            
         
-    def match(self, other):
-        """Changes block to be the same width x offset of other block"""
-        self.origin = (other.origin[0], self.origin[1])  # set x param to be same
-        self.width = other.width
+#        self.origin = (other.origin[0], self.origin[1])  # set x param to be same
+#        self.width = other.width
         
     def move(self, direction):
         """Moves block on step in given direction"""
         if direction == Direction.LEFT:
-            self.origin = (self.origin[0]-1, self.origin[1])
+            self.origin = [self.origin[0]-1, self.origin[1]]
         elif direction == Direction.RIGHT:
-            self.origin = (self.origin[0]+1, self.origin[1])
+            self.origin = [self.origin[0]+1, self.origin[1]]
         elif direction == Direction.UP:
-            self.origin = (self.origin[0], self.origin[1]+1)
+            self.origin = [self.origin[0], self.origin[1]+1]
         elif direction == Direction.DOWN:
-            self.origin = (self.origin[0], self.origin[1]-1)
+            self.origin = [self.origin[0], self.origin[1]-1]
         else:
             raise RuntimeError("Invalid Direction")
 
 try:
     # SETUP ==========================
 
+    # setup led matrix
+    led2.init_grid(2,1,270)
+    HEIGHT = led2.height()
+    WIDTH = led2.width()
+    
+    # initialize variables
+    curr_state = State.IDLE  # current state used for state machine
+    blocks = []              # current block elements on screen
+    start_width = 3           # pixel width of block on start
+    start_speed = 5           # current speed on start (in pixel/sec)
+    change_level = int(HEIGHT*(1/3.))  # number of blocks before upping difficulty
+    curr_width, curr_speed = start_width, start_speed
+    curr_direction = Direction.RIGHT  # current direction top block is moving
+    
+    last_input = 1
+    last_time = 0
+    locked = False  # spin lock for interrupt handler
+    
+    # helper functions
+    def draw_blocks():
+        """Draws the current blocks on screen"""
+        for block in blocks:
+            block.draw()
+
     # setup gpio buttons
-    BUTTON=2  # GPIO pin button is connected to
+    BUTTON=4  # GPIO pin button is connected to
     def button_handler(channel):
-        if channel == BUTTON:
+        global curr_state
+        global curr_speed
+        global curr_width
+        global curr_direction
+        global blocks
+        if not locked:
             if curr_state == State.PLAYING:
                 # stop the block
                 if len(blocks) > 1:
                     if blocks[-1].adjacent(blocks[-2]):
-                        blocks[-1].match(blocks[-2])  # make block same size and position
+                        blocks[-1] = blocks[-1].crop(blocks[-2])
                     else:
                         curr_state = State.LOSE
                         return
@@ -82,7 +129,7 @@ try:
                 # show stopped block for a couple seconds
                 draw_blocks()
                 led2.show()
-                time.sleep(1)
+#                time.sleep(1)
                 
                 # if hit the ceiling, display win screen 
                 if len(blocks) == HEIGHT:
@@ -93,45 +140,35 @@ try:
                 if len(blocks) % change_level == 0:  # check if to up difficulty
                     if curr_width > 1:
                         curr_width -= 1
-                    curr_speed += 1  # update current speed
+                    curr_speed += 5  # update current speed
                 curr_width = min(blocks[-1].width, curr_width)
-                blocks.append(Block((0, blocks[-1].origin[1]+1), curr_width))
+                blocks.append(Block([0, blocks[-1].origin[1]+1], curr_width))
                 
             elif curr_state in [State.IDLE, State.WIN, State.LOSE]:
                 # set up game
-                blocks = [Block((0,0), start_width, start_speed)]  # set up first block
+                blocks = [Block([0,0], start_width)]  # set up first block
+                curr_width = start_width
+                curr_speed = start_speed
                 led2.erase()
                 draw_blocks()
                 led2.show()
                 curr_state = State.PLAYING
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.add_event_detect(BUTTON, GPIO.FALLING, callback=button_handler, bouncetime=100)
+    GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(BUTTON, GPIO.FALLING, callback=button_handler, bouncetime=300)
     
-    # setup led matrix
-    led2.init_grid(2,1,180)
-    HEIGHT = led2.height()
-    WIDTH = led2.width()
-    
-    # initialize variables
-    curr_state = State.IDLE  # current state used for state machine
-    blocks = []              # current block elements on screen
-    start_width = 3           # pixel width of block on start
-    start_speed = 2           # current speed on start (in pixel/sec)
-    start_level = 1           # level of difficulty to start with 
-    change_level = int(HEIGHT*(1/3.))  # number of blocks before upping difficulty
-    curr_width, curr_speed = start_width, start_speed
-    curr_direction = Direction.RIGHT  # current direction top block is moving
 
-    # helper functions
-    def draw_blocks():
-        """Draws the current blocks on screen"""
-        for block in blocks:
-            block.draw()
             
     # State Machine ==================
     
     while curr_state != State.DONE:
+#        # check if button pressed
+#        if (not GPIO.input(BUTTON)) and ((time.time() - last_time) > .3) and (last_input == 1):
+#            button_handler(BUTTON)
+#            last_time = time.time()
+#        last_input = GPIO.input
+        
         if curr_state == State.IDLE:
             # TODO: do something cooler
             led2.point(0,0)
@@ -143,36 +180,40 @@ try:
         elif curr_state == State.PLAYING:
             # change direction if hit edge of screen
             if curr_direction == Direction.RIGHT:
-                if blocks[-1].origin[0] + blocks.width == WIDTH-1:
+                if blocks[-1].origin[0] + blocks[-1].width == WIDTH:
                     curr_direction = Direction.LEFT
             elif blocks[-1].origin[0] == 0:
                 curr_direction = Direction.RIGHT
                 
             # move top block in curr_direction at curr_speed
+            locked = True
             blocks[-1].move(curr_direction)
+            locked = False
             led2.erase()
             draw_blocks()
             led2.show()
             time.sleep(1/curr_speed)
             
         elif curr_state == State.WIN:
+            print("IN WIN")
             led2.erase()
             led2.text("WIN!", font_size="small")
             led2.show()
-            time.sleep(3)
+            time.sleep(1)
 
         elif curr_state == State.LOSE:
             # blink failing block
+            print("IN LOSE")
             blocks[-1].color = 0
             led2.erase()
             draw_blocks()
             led2.show()
-            time.sleep(1)
+            time.sleep(.2)
             led2.erase()
             blocks[-1].color = 0xF
             draw_blocks()
             led2.show()
-            time.sleep(1)
+            time.sleep(.2)
 
         else:
             raise RuntimeError("Invalid State")
