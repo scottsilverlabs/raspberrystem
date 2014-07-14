d457ffe0905aee1bb5d2587887ffc89c999abef1
Stephan Wolski <me@stephanwolski.com>
2014-07-14 14:55:53 -0400

    Added privilege check

--- scripts/configuredistro.sh
+++ scripts/configuredistro.sh
@@ -1,9 +1,13 @@
-sudo apt-get -y autoremove wolfram-engine dillo lxsession lxterminal lightdm scratch sonic-pi rsync cron triggerhappy #Remove uneeded programs 
-sudo apt-get -y install chromium
-sudo sed -i "s/exit 0\/sudo \/usr\/bin\/ideserver &\nnetpd -gqx &\nexit 0/" /etc/rc.local #Start IDE server and set time on boot
-sudo update-rc.d -f ntp disable #Disable the time daemon, it's pointless on the Pi
-sudo sed -i "s/1:2345:respawn:\/sbin\/getty --noclear 38400 tty1/1:2345:respawn:\/bin\/login -f root tty1 <\/dev\/tty1 >\/dev\/tty1 2>&1/" #Have TTY1 auto login to root
-sudo echo '#!/bin/bash
+if [ "$USER" = "root" ]; then
+	apt-get update
+	apt-get -y dist-upgrade
+	apt-get -y autoremove wolfram-engine pistore dillo lxsession lxterminal lightdm scratch sonic-pi rsync cron triggerhappy #Remove uneeded programs 
+	apt-get -y install chromium
+	update-rc.d ssh start
+	sed -i "s/^exit 0/sudo \/usr\/bin\/ideserver \&\nnetpd -gqx \&\nexit 0/" /etc/rc.local #Start IDE server and set time on boot
+	update-rc.d -f ntp disable #Disable the time daemon, it's pointless on the Pi
+	sed -i "s/1:2345:respawn:\/sbin\/getty --noclear 38400 tty1/1:2345:respawn:\/bin\/login -f root tty1 <\/dev\/tty1 >\/dev\/tty1 2>\&1/" /etc/inittab #Have TTY1 auto login to root
+	echo '#!/bin/bash
 if [[ -z $DISPLAY && $(tty) = /dev/tty1 ]]; then
         whiptail --title "IDE" --yesno "Launch the IDE?" 7 20
         if [ "$?" = "0" ]; then
@@ -13,10 +17,14 @@ if [[ -z $DISPLAY && $(tty) = /dev/tty1 ]]; then
         fi
 	cat /etc/null > .config/openbox/autostart
 fi' > /root/.profile #Ask to boot into the IDE if there is no X server and they are in TTY1
-sudo cat /dev/null > /etc/X11/default-display-manager #default-display-manager is lightdm, which was removed
-sed -i "/^overscan_.*/d" /boot/config.txt
-sed -i "s/^#disable_overscan/disable_overscan/" /boot/config.txt
-sed -i "s/^gpu_mem=.*/gpu_mem=16/" /boot/config.txt
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-sudo locale-gen
-sudo dpkg-reconfigure keyboard-configuration
+	cat /dev/null > /etc/X11/default-display-manager #default-display-manager is lightdm, which was removed
+	sed -i "s/^#disable_overscan/disable_overscan/" /boot/config.txt
+	echo "gpu_mem=16" >> /boot/config.txt
+	echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+	export LANG="en_US.UTF-8"
+	echo 'export LANG=en_US.UTF-8' >> /root/.bashrc
+	sed -i 's/XKBLAYOUT="gb"/XKBLAYOUT="us"/' /etc/default/keyboard
+	locale-gen
+else
+	echo "Run as root"
+fi
--- scripts/optimizedistro.sh
+++ scripts/optimizedistro.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove --purge rsyslog
-sudo apt-get -y install dropbear openssh-client inetutils-syslogd
-sudo apt-get clean
-sed -i '/[3-6]:23:respawn:\/sbin\/getty 38400 tty[2-6]/s%^%#%g' /etc/inittab #Dsable getty
-dpkg-reconfigure dash #Replace Bash with Dash
-/etc/init.d/ssh stop #Stop SSH
-sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear #Enable dropbear on boot
-/etc/init.d/dropbear start #Start dropbear
-update-rc.d ssh disable #Stop SSH on boot
-sed -i 's/defaults,noatime/defaults,noatime,nodiratime/g' /etc/fstab #Optimize mounting
-sed -i 's/deadline/noop/g' /boot/cmdline.txt #Switch to better scheduler
-service inetutils-syslogd stop #Stop the logging daemon
-##Delete old logs##
-for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog; do [ -f "$file" ] && rm -f "$file"; done
-for dir in fsck news; do [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"; done
-####
-echo -e "*.*;mail.none;cron.none\t -/var/log/messages\ncron.*\t -/var/log/cron\nmail.*\t -/var/log/mail" > /etc/syslog.conf #Create a basic config
-mkdir -p /etc/logrotate.d
-echo -e "/var/log/cron\n/var/log/mail\n/var/log/messages {\n\trotate 4\n\tweekly\n\tmissingok\n\tnotifempty\n\tcompress\n\tsharedscripts\n\tpostrotate\n\t/etc/init.d/inetutils-syslogd reload >/dev/null\n\tendscript\n}" > /etc/logrotate.d/inetutils-syslogd #Create a basic logrotate config
-service inetutils-syslogd start #Start the new daemon
-sudo rpi-update
-shutdown -r now
+if [ "$USER" = "root" ]; then
+	sudo apt-get update
+	sudo apt-get -y dist-upgrade
+	sudo apt-get -y autoremove --purge rsyslog
+	sudo apt-get -y install dropbear openssh-client inetutils-syslogd
+	sudo apt-get clean
+	sed -i "/[3-6]:23:respawn:\/sbin\/getty 38400 tty[2-6]/s%^%#%g" /etc/inittab #Dsable getty
+	dpkg-reconfigure dash #Replace Bash with Dash
+	/etc/init.d/ssh stop #Stop SSH
+	sed -i "s/NO_START=1/NO_START=0/g" /etc/default/dropbear #Enable dropbear on boot
+	/etc/init.d/dropbear start #Start dropbear
+	update-rc.d ssh disable #Stop SSH on boot
+	sed -i "s/defaults,noatime/defaults,noatime,nodiratime/g" /etc/fstab #Optimize mounting
+	sed -i "s/deadline/noop/g" /boot/cmdline.txt #Switch to better scheduler
+	service inetutils-syslogd stop #Stop the logging daemon
+	##Delete old logs##
+	for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog; do [ -f "$file" ] && rm -f "$file"; done
+	for dir in fsck news; do [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"; done
+	####
+	echo -e "*.*;mail.none;cron.none\t -/var/log/messages\ncron.*\t -/var/log/cron\nmail.*\t -/var/log/mail" > /etc/syslog.conf #Create a basic config
+	mkdir -p /etc/logrotate.d
+	echo -e "/var/log/cron\n/var/log/mail\n/var/log/messages {\n\trotate 4\n\tweekly\n\tmissingok\n\tnotifempty\n\tcompress\n\tsharedscripts\n\tpostrotate\n\t/etc/init.d/inetutils-syslogd reload >/dev/null\n\tendscript\n}" > /etc/logrotate.d/inetutils-syslogd #Create a basic logrotate config
+	service inetutils-syslogd start #Start the new daemon
+	sudo rpi-update
+	shutdown -r now
+else
+	echo "Run as root"
+fi
