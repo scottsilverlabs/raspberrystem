4a796eeb775421b9c9b40c472e05824079e83026
Stephan Wolski <me@stephanwolski.com>
2014-07-15 10:50:00 -0400

    Fixed spi kernel module name

--- scripts/configuredistro.sh
+++ scripts/configuredistro.sh
@@ -4,7 +4,7 @@ if [ "$USER" = "root" ]; then
 	apt-get -y autoremove wolfram-engine pistore dillo lxsession lxterminal lightdm scratch sonic-pi rsync cron triggerhappy #Remove uneeded programs 
 	apt-get -y install chromium
 	update-rc.d ssh enable
-	echo "spi-dev\ni2c-dev" >> /etc/modules
+	echo "spidev\ni2c-dev" >> /etc/modules
 	sed -i "s/^exit 0/sudo \/usr\/bin\/ideserver \&\nnetpd -gqx \&\nexit 0/" /etc/rc.local #Start IDE server and set time on boot
 	update-rc.d -f ntp disable #Disable the time daemon, it's pointless on the Pi
 	sed -i "s/1:2345:respawn:\/sbin\/getty --noclear 38400 tty1/1:2345:respawn:\/bin\/login -f root tty1 <\/dev\/tty1 >\/dev\/tty1 2>\&1/" /etc/inittab #Have TTY1 auto login to root
@@ -26,7 +26,7 @@ fi' > /root/.profile #Ask to boot into the IDE if there is no X server and they 
 	echo 'export LANG=en_US.UTF-8' >> /root/.bashrc
 	echo 'export LANG=en_US.UTF-8' >> .bashrc
 	sed -i 's/XKBLAYOUT="gb"/XKBLAYOUT="us"/' /etc/default/keyboard
-	locale-gen
+	locale-gen en_US en_US.UTF-8
 else
 	echo "Run as root"
 fi
