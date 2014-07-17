ff6b6e800c1e5a8bc0a6e62cd3a232a9a0797d55
Jon Snyder <jonsnyder1@gmail.com>
2014-07-17 16:36:34 -0400

    small changes after testing

--- rstem/button/button.py
+++ rstem/button/button.py
@@ -13,23 +13,18 @@ def cleanup():
 
 class Button(object):
 
-    def __init__(self, port, pressed_is_high=True):
-        if pressed_is_high:
-            pull_up_down = GPIO.PUD_DOWN
-        else:
-            pull_up_down = GPIO.PUD_UP
+    def __init__(self, port):
         self.port = port
-        self.pressed_is_high = pressed_is_high
-        GPIO.setup(port, GPIO.IN, pull_up_down=pull_up_down)
+        GPIO.setup(port, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
     def is_pressed(self):
-        if self.pressed_is_high:
-            return GPIO.input(self.port)
+        if GPIO.input(self.port):
+            return False
         else:
-            return (not GPIO.input(self.port))
+            return True
         
-    def call_when_pressed(self, event_callback, bouncetime=200):
-        GPIO.add_event_callback(self.port, callback=event_callback, bouncetime=bouncetime)
+    def call_when_pressed(self, event_callback, bouncetime=300):
+        GPIO.add_event_detect(self.port, GPIO.FALLING, callback=event_callback, bouncetime=bouncetime)
         
     def wait_for_edge(self, edge):
         if edge not in [FALLING, RISING, BOTH]:
