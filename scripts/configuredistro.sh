f627968e790b5633dab3bdc4312ccf19aac5c2ed
Stephan Wolski <me@stephanwolski.com>
2014-07-17 09:45:33 -0400

    Added config directory creation

--- scripts/configuredistro.sh
+++ scripts/configuredistro.sh
@@ -4,7 +4,7 @@ if [ "$USER" = "root" ]; then
 	apt-get -y autoremove wolfram-engine pistore dillo lxsession lxterminal lightdm scratch sonic-pi rsync cron triggerhappy #Remove uneeded programs 
 	apt-get -y install chromium
 	update-rc.d ssh enable
-	echo "spidev\ni2c-dev" >> /etc/modules
+	echo "spidev\ni2c-dev\nbcm2835-v4l2" >> /etc/modules
 	sed -i "s/^exit 0/sudo \/usr\/bin\/ideserver \&\nnetpd -gqx \&\nexit 0/" /etc/rc.local #Start IDE server and set time on boot
 	update-rc.d -f ntp disable #Disable the time daemon, it's pointless on the Pi
 	sed -i "s/1:2345:respawn:\/sbin\/getty --noclear 38400 tty1/1:2345:respawn:\/bin\/login -f root tty1 <\/dev\/tty1 >\/dev\/tty1 2>\&1/" /etc/inittab #Have TTY1 auto login to root
@@ -19,6 +19,7 @@ if [[ -z $DISPLAY && $(tty) = /dev/tty1 ]]; then
 	cat /etc/null > .config/openbox/autostart
 fi' > /root/.profile #Ask to boot into the IDE if there is no X server and they are in TTY1
 	cat /dev/null > /etc/X11/default-display-manager #default-display-manager is lightdm, which was removed
+	mkdir -p /root/.config/openbox
 	sed -i "s/^#disable_overscan/disable_overscan/" /boot/config.txt
 	echo "gpu_mem=16" >> /boot/config.txt
 	echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
