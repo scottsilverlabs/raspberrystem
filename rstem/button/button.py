1c34e04167e7c53f29cc8618708fd416277e05b2
Brian Silverman <b.scottsilver@gmail.com>
2014-07-24 07:32:29 -0400

    Minor style changes, untested

--- rstem/button/button.py
+++ rstem/button/button.py
@@ -18,17 +18,16 @@ class Button(object):
         GPIO.setup(port, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
     def is_pressed(self):
-        if GPIO.input(self.port):
-            return False
-        else:
-            return True
-        
-    def call_on_change(self, event_callback, change=PRESSED, bouncetime=300):
+        return not bool(GPIO.input(self.port)):
+
+    def _verify_change_value(change):
         if change not in [PRESSED, RELEASED, BOTH]:
             raise ValueError("Invalid change")
+        
+    def call_on_change(self, event_callback, change=PRESSED, bouncetime=300):
+        _verify_change_value(change)
         GPIO.add_event_detect(self.port, change, callback=event_callback, bouncetime=bouncetime)
         
     def wait_for_change(self, change):
-        if change not in [PRESSED, RELEASED, BOTH]:
-            raise ValueError("Invalid change")
+        _verify_change_value(change)
         GPIO.wait_for_edge(self.port, change)
