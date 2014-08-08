b3b012ef3c20325b2db6c5038602db10b3623179
Jon Snyder <jonsnyder1@gmail.com>
2014-08-08 14:07:09 -0400

    add copyright and move sprite files to dice_sprites folder

--- projects/led_matrix_games/dice.py
+++ projects/led_matrix_games/dice.py
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2014, Scott Silver Labs, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from rstem import led_matrix, accel
 import RPi.GPIO as GPIO
@@ -13,10 +28,13 @@ led_matrix.init_grid()
 # set up accelerometer
 accel.init(1)
 
+# set up GPIO
+GPIO.setmode(GPIO.BCM)
+
 # set up dice sprites
 dice = []
 for value in range(1,7):
-    dice.append(led_matrix.LEDSprite(os.path.abspath(str(value) + ".spr")))
+    dice.append(led_matrix.LEDSprite(os.path.abspath("dice_sprites/" + str(value) + ".spr")))
     
 # set up buttons
 UP = 25
@@ -29,7 +47,6 @@ START = 27
 SELECT = 22
 
 # setup start button to exit game
-GPIO.setmode(GPIO.BCM)
 GPIO.setup(START, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # setup A button to roll dice
@@ -51,9 +68,10 @@ while True:
         GPIO.cleanup()
         sys.exit(0)
     
-    # roll dice if A button is pressed
+    # roll dice if A button is pressed or accelerometer detects steep enough angle
     if just_started or GPIO.input(A) == 0 or abs(accel.angles()[2] - base_elevation) > THRESHOLD:
         led_matrix.erase()  # clear old dice values
+        # set a new random die at each matrix
         for y in range(0, led_matrix.height(), 8):
             for x in range(0, led_matrix.width(), 8):
                 led_matrix.sprite(random.choice(dice), (x+1,y+1))
