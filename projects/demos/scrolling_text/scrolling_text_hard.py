538088ced190b54b697df787ab7fd70cd3b20f16
Jon Snyder <jonsnyder1@gmail.com>
2014-08-18 19:35:49 -0400

    add hard instructions

--- projects/demos/scrolling_text/scrolling_text_hard.py
+++ projects/demos/scrolling_text/scrolling_text_hard.py
@@ -7,42 +7,37 @@ led_matrix.erase()      # This clears the led matrix display incase anything is 
 
 # Scrolling the led matrix. ===============================================
 
-
+# Create a variable that holds information about the sprite
 my_text_sprite = led_matrix.LEDText("Hello World")
-# Note: my_text_sprite is a variable that holds information on how to display your text.
 
-# 5. Make a while loop that keeps looping forever
+# Make a while loop that keeps looping forever
 while True:  # by making the conditional True, the while loop will never break because True is always True!
    
-   # 6. Get the width of the led matrix display.
-   display_width = led_matrix.width()   # this is the number of LEDs wide the display is.
-   
-   # 7. Get the width of the text in pixels.
-   text_width = my_text_sprite.width   # this is the number of LEDs wide our my_text_sprite is.
-   
-    # 8. Set the x position to be originally all the way to the right of the screen, by using the text_width variable we made.
-    x = text_width
+    # Get the width of the text in pixels.
+    text_width = my_text_sprite.width   # this is the number of LEDs wide our my_text_sprite is.
+
+    # Set the original x value to be the width of the led matrix display.
+    x = led_matrix.width()   # this is the number of LEDs wide the display is. 
     
-    
-    # 9. Make another while loop that keeps moving the text to the left.
+    # Make another while loop that keeps moving the text to the left.
     #   - When x is less then -text_width, our text will not be on the display anymore.
     #   - When this happens we want to stop this while loop and run through the outside while loop.
     while x > -text_width:   # when x is less then -text_width, our text will not be on the display anymore
         
-        # 11. Erase the previous drawn text
+        # Erase the previous drawn text
         led_matrix.erase()
         
-        # 12. Draw the text at this current x position
+        # Draw the text at this current x position
         led_matrix.sprite(text, (x, 0))
         
-        # 13. Show the text on the screen
+        # Show the text on the screen
         led_matrix.show()
         
-        # 14. Subtract 1 from x, so the next loop around moves the text one more to the left.
-        x = x - 1
+        # Hold for a second so we can see the text. (If we don't do this the text will move too quickly to see it.)
+        time.sleep(.1)  # We will wait 1 second before going through the loop again.
         
-        # 15. Hold for a second so we can see the text. (If we don't do this the text will move too quickly to see it.)
-        time.sleep(1)  # We will wait 1 second before going through the loop again.
+        # Subtract 1 from x, so the next loop around moves the text one more to the left.
+        x = x - 1
         
     
 # Yay!! Now our text is continuously scrolling to the left.
