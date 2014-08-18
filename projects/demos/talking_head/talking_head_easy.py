9457d312a1477ddc43570ecfb87544e0382390bb
Jon Snyder <jonsnyder1@gmail.com>
2014-08-18 19:45:35 -0400

    create an easy that just does the text to speech without the led matrix, make original talking head project has medium, TODO make a hard that is a soundboard of different speeches with GPIO buttons.

---
+++ projects/demos/talking_head/talking_head_easy.py
@@ -0,0 +1,9 @@
+from rstem import speaker
+import time
+import os
+
+# Make the RaspberryPi say "Hello World"
+speaker.say("Hello World")
+
+
+# TODO: This project may be tooo easy... make user input thing???    
