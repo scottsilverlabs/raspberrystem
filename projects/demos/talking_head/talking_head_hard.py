dec6ee3c2d1ed429946335f8476e809cfa4263b2
Jon Snyder <jonsnyder1@gmail.com>
2014-08-19 13:39:28 -0400

    add and develop easy and hard instructions

--- projects/demos/talking_head/talking_head_hard.py
+++ projects/demos/talking_head/talking_head_hard.py
@@ -1,4 +1,4 @@
-from rstem import led_matrix, speaker
+from rstem import led_matrix, speaker, button
 import time
 import os
 
@@ -8,40 +8,65 @@ led_matrix.erase()      # This clears the led matrix display incase anything is 
 # TODO: Create a soundboard of different speeches by that are activated by pressing buttons....
 
 
+# TODO: better (simpler to understand, yet elegant) way to do this?
+# TODO: use less buttons?
+# create a dictionary that matches the buttons (at ports 4, 17, 25, 24, 23, 18, 27, and 22) to the sound
+soundboard = {
+    button.Button(4) : speaker.Speech("Hello World"),
+    button.Button(17) : speaker.Speech("How are you?"),
+    button.Button(25) : speaker.Speech("RaspberrySTEM is super cool!"),
+    button.Button(24) : speaker.Speech("The first 16 digits of pi is 3.141592653589793"),
+    button.Button(23) : speaker.Speech("Always bring a banana to a party!"),
+    button.Button(18) : speaker.Speech("I'm sorry Dave, I'm afraid I can't do that."),
+    button.Button(27) : speaker.Speech("I am functioning within normal parameters."),
+    button.Button(22) : speaker.Speech("EXTERMINATE!")
+}
 
-## 1. Create sprite variables of a face with mouth open and closed from .spr files.
-#mouth_closed = led_matrix.LEDSprite(os.path.abspath("mouth_closed.spr"))
-#mouth_open = led_matrix.LEDSprite(os.path.abspath("mouth_open.spr"))
-
-## 2. Create a speech object of "Hello World"
-#my_speech = speaker.Speech("Hello World")
-
-## 3. Play my_speech
-#my_speech.play()
-
-## 4. Create a while loop that keeps looping until the Raspberry Pi has stopped talking.
-#while my_speech.is_playing():
-
-#    # 5. Clear the led matrix
-#    led_matrix.erase()
-#    
-#    # 6. Draw the face with its mouth open
-#    led_matrix.sprite(mouth_open)
-#    
-#    # 7. Show the new face on the display and add a delay
-#    led_matrix.show()
-#    time.sleep(.1)
-#    
-#    # 8. Clear the led matrix of the previous face
-#    led_matrix.erase()
-#    
-#    # 9. Draw the face with its mouth closed
-#    led_matrix.sprite(mouth_closed)
-#    
-#    #10. Show the new face on the display and add a delay
-#    led_matrix.show()
-#    time.sleep(.1)
     
+# Create sprite variables of a face with mouth open and closed from .spr files.
+mouth_closed = led_matrix.LEDSprite(os.path.abspath("mouth_closed.spr"))
+mouth_open = led_matrix.LEDSprite(os.path.abspath("mouth_open.spr"))
+
+# create a while loop that will loop inifinitly by setting the conditional to True
+while True:
+
+    # originally set the speech to None because we haven't picked a speech to play yet
+    my_speech = None
+    
+    # pick the first speech in which the button is pressed
+    while my_speech is None:
+        for button in soundboard:
+            if button.is_pressed():
+                my_speech = soundboard[button]
+                break
+
+
+    # Play my_speech
+    my_speech.play()
+
+    # Create a while loop that keeps looping until the Raspberry Pi has stopped talking.
+    while my_speech.is_playing():
+
+        # Clear the led matrix
+        led_matrix.erase()
+        
+        # Draw the face with its mouth open
+        led_matrix.sprite(mouth_open)
+        
+        # Show the new face on the display and add a delay
+        led_matrix.show()
+        time.sleep(.1)
+        
+        # Clear the led matrix of the previous face
+        led_matrix.erase()
+        
+        # Draw the face with its mouth closed
+        led_matrix.sprite(mouth_closed)
+        
+        # Show the new face on the display and add a delay
+        led_matrix.show()
+        time.sleep(.1)
+        
     
     
     
