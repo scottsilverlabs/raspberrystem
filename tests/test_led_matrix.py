bc5495e1c5bed4bab83244ba713d020d580cd278
Jon Snyder <jonsnyder1@gmail.com>
2014-08-22 16:48:10 -0400

    change from using unittext to py.test, created some simple tests to make sure init_grid is working correctly

--- tests/test_led_matrix.py
+++ tests/test_led_matrix.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
-import unittest
+import pytest
 import os
-from rstem import led2
+from rstem import led_matrix
 
 # TODO: single setup for all testcases????
 # TODO: make hardware versions that you can optionally skip
 
-if __name__ == '__main__':
-    unittest.main()
-    
 def query(question):
     ret = int(input(question + "  [yes=1,no=0]: "))
     if ret == 1:
@@ -18,74 +15,54 @@ def query(question):
         return False
     else:
         raise ValueError("Please only provide 1 or 0. Thank You!")
+        
 
-class PrivateFunctions(unittest.TestCase):
-    
-    def test_valid_color(self):
-        for i in range(16):
-            self.assertTrue(led2.valid_color(i))
-            self.assertTrue(led2.valid_color(hex(i)[2:]))
-        self.assertTrue(led2.valid_color('-'))
-        self.assertFalse(led2.valid_color(17))
-        self.assertFalse(led2.valid_color(-1))
-        # random invalid characters
-        self.assertFalse(led2.valid_color('j'))
-        self.assertFalse(led2.valid_color('%'))
-        self.assertFalse(led2.valid_color('10'))  #TODO : should we allow none hex versions?
+def test_valid_color():
+    for i in range(16):
+        assert led_matrix._valid_color(i)
+        assert led_matrix._valid_color(hex(i)[2:])
+    assert led_matrix._valid_color('-')
+    assert led_matrix._valid_color(17)
+    assert led_matrix._valid_color(-1) == 0
+    # random invalid characters
+    assert led_matrix._valid_color('j') == 0
+    assert led_matrix._valid_color('%') == 0
+    assert led_matrix._valid_color('10') == 0
         
-    def testconvert_color(self):
-        self.assertRaises(ValueError, led2.convert_color('J')) # FIXME: syntax correct?
-        for i in range(16):
-            self.assertEquals(led2.convert_color(hex(i)[2:]), i)
-            self.assertEquals(led2.convert_color(i), i)
+def test_convert_color():
+    with pytest.raises(ValueError):
+        led_matrix.convert_color('J')
+    for i in range(16):
+        assert led_matrix._convert_color(hex(i)[2:]) == i
+        assert led_matrix._convert_color(i) == i
+        
+def test_init_grid():
+    led_matrix.cleanup()
+    for math_coords in [True, False]:
+        for angle in [0, 90, 180, 270]:
+            led_matrix.cleanup()
+            led_matrix.init_grid(angle=angle, math_coords=math_coords)
+            for y in range(0, led_matrix.height(), 8):
+                for x in range(0, led_matrix.width(), 8):
+                    led_matrix.text(str(x + x*y), (x,y))
+            led_matrix.show()
+            if math_coords:
+                assert query("Do you see numbers (rotated correctly) going from left to right and bottom to top?")
+            else:
+                assert query("Do you see numbers (rotated correctly) going from left to right and top to bottom?")
+                
+            try:
+                input("Please rotate the screen 90 degres then press ENTER.")
+            except SyntaxError:
+                pass
+    
 
-class PrimativeFunctions(unittest.TestCase):
-    
-    def setUp(self):
-        led2.init_grid(1,2)
         
-    def tearDown(self):
-        led2.shutdown_matrices()
+def test_line(self):
+    led_matrix.init_grid()
+    led_matrix.line((0,0), (led_matrix.width()-1, led_matrix.height()-1))
+    led_matrix.show()
+    assert query("Is there a line from (0,0) to (%s,%s)?" % led_matrix.widht()-1, led_matrix.height()-1)
         
-    def test_point(self):
-        for y in range(8):
-            for x in range(16):
-                # test without a color
-                self.assertEquals(led2.point(x,y), 1)
-                self.assertEquals(led2.point((x,y)), 1)
-                # test with all the colors
-                for color in range(17):
-                    self.assertEquals(led2.point(x,y), 1)
-                    self.assertEquals(led2.point((x,y)), 1)
-        self.assertEquals(led2.show(), 1)
-        self.assertTrue(query("Is the entire matrix at full brightness?"))
-        
-    def test_line(self):
-        self.assertEquals(led2.line((0,0),(15,7), 1), 1)
-        self.assertEquals(led2.show(), 1)
-        self.assertTrue(query("Is there a line from (0,0) to (15,7)?"))
-        
-class TestingSprites(unittest.TestCase):
-    
-    def setUp(self):
-        led2.init_grid(1,2)
-        
-    def tearDown(self):
-        led2.shutdown_matrices()
-        
-    def test_init_sprite(self):
-        if font_path is None: # if none, set up default font location
-            this_dir, this_filename = os.path.split(__file__)
-        self.assertEquals(led2.sprite(this_dir + "/test_sprite"), 1)
-        self.assertEquals(led2.show(), 1)
-        self.assertTrue(query("Do you see the test pattern on the left led matrix?"))
-        self.assertEquals(led2.fill(0), 1)
-        self.assertEquals(led2.show(), 1)
-        self.assertFalse(query("What about now?"))
-        
-    def test_text(self):
-        self.assertNotEquals(led2.text("jon"), 0)
-        self.assertEquals(led2.show(), 1)
-        self.assertTrue(query("Is 'jon' displayed with large font?"))
         
         
