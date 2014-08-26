f991308d9c05e111ef8404661b65189a448e5c85
Joe Wolski <joe@raspberrystem.com>
2014-08-26 15:10:58 -0400

    Added varable ANGLE_FACTOR to adjust field of view

--- projects/led_matrix_camera/Greyscale.py
+++ projects/led_matrix_camera/Greyscale.py
@@ -2,18 +2,22 @@
 import numpy as np
 import cv2
 from rstem import led_matrix
-GRID_SIZE = 2
+ANGLE_FACTOR = 1
+GRID_SIZE = 6
+IMAGE_SIZE = 8*GRID_SIZE*ANGLE_FACTOR
 cap = cv2.VideoCapture(0)
-cap.set(3, 8*GRID_SIZE) #width
-cap.set(4, 8*GRID_SIZE) #height
-cap.set(11, 1) #max exposure
-led_matrix.init_grid(GRID_SIZE)
+cap.set(3, IMAGE_SIZE) #width
+cap.set(4, IMAGE_SIZE) #height
+cap.set(11, 1) #max contrast
+led_matrix.init_grid(GRID_SIZE, GRID_SIZE)
+offset = int(round( (IMAGE_SIZE-(8*GRID_SIZE))/2 ))
 try:
 	while True:
 		led_matrix.fill(0)
 		ret, img = cap.read()
 		img = cv2.flip(img, 0)
-		grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		new_img = img[offset:offset+8*GRID_SIZE,offset:offset+8*GRID_SIZE]
+		grey = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
 		grey = np.round(np.divide(grey, 16))
 		led_matrix.frame(grey)
 		led_matrix.show()
