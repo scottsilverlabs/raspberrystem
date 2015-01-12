1459027f4766dd21a1ba27aaf2f1387457f41c68
Brian Silverman <bri@raspberrystem.com>
2015-01-12 16:51:21 -0500

    gpio - added button testing

--- rstem/tests/test_auto_gpio_loopback_23_to_24.py
+++ rstem/tests/test_auto_gpio_loopback_23_to_24.py
@@ -15,55 +15,58 @@ Short GPIO 23 to 24.
 OUTPUT_PIN = 23
 INPUT_PIN = 24
 
-def io_setup(func):
-    @wraps(func)
-    def wrapper():
-        # Setup
-        i = g.Input(INPUT_PIN)
-        o = g.Output(OUTPUT_PIN)
+def io_setup(button=False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper():
+            # Setup
+            InputFunc = g.Button if button else g.Input
+            i = InputFunc(INPUT_PIN)
+            o = g.Output(OUTPUT_PIN)
 
-        passed = func(i, o)
+            passed = func(i, o)
 
-        # Teardown
-        g.Input(OUTPUT_PIN)
-        return passed
-    return wrapper
+            # Teardown
+            g.Input(OUTPUT_PIN)
+            return passed
+        return wrapper
+    return decorator
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_starts_off(i, o):
     return i.is_off()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_turned_on(i, o):
     o.off()
     o.on()
     return i.is_on()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_turned_off(i, o):
     o.on()
     o.off()
     return i.is_off()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_turned_on_via_set(i, o):
     o.off()
     o.level = 1
     return i.is_on()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_turned_off_via_set(i, o):
     o.on()
     o.level = 0
     return i.is_off()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def io_on_off_sequence(i, o):
     on_times = 0
     off_times = 0
@@ -81,41 +84,41 @@ def io_on_off_sequence(i, o):
     return passed
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_init_start_off_false(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def output_init_active_low_false(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_is_off(i, o):
     o.off()
     return i.is_off()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_is_on(i, o):
     o.off()
     return i.is_off()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_is_off_via_get(i, o):
     o.on()
     return i.level
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_is_on_via_get(i, o):
     o.off()
     return not i.level
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_is_on_via_get_triplet(i, o):
     # TBD
     # Add optional parameter to get() triplet of falling, rising, and current
@@ -124,64 +127,64 @@ def input_is_on_via_get_triplet(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_wait_for_change(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_wait_for_change_with_timeout(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_call_if_changed(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_call_if_changed_disable(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_init_active_low_false(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_init_poll_period_slow(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_init_pullup(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_init_pulldown(i, o):
     return False
 
 @testing.automatic
-@io_setup
+@io_setup()
 def input_init_pullnone(i, o):
     return False
 
 @testing.automatic
-@io_setup
-def input_button_is_pressed(i, o):
-    o.off()
-    return i.is_pressed()
+@io_setup(button=True)
+def input_button_is_pressed(b, o):
+    o.on()
+    return b.is_pressed()
 
 @testing.automatic
-@io_setup
-def input_button_is_released(i, o):
+@io_setup(button=True)
+def input_button_is_released(b, o):
     o.off()
-    return i.is_released()
+    return b.is_released()
 
 @testing.automatic
-@io_setup
+@io_setup()
 def time_output(i, o):
     TRIES = 100
     start = time.time()
@@ -196,7 +199,7 @@ def time_output(i, o):
     return rate > MINIMUM_RATE
 
 @testing.automatic
-@io_setup
+@io_setup()
 def time_input(i, o):
     TRIES = 100
     start = time.time()
