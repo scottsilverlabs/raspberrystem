e15cd9e34a5b78fc6cf2d3b25b755f1b107ce8b9
Brian Silverman <bri@raspberrystem.com>
2015-01-29 10:26:44 -0500

    led matrix - deprecated old tests
    Left the tests in place, but will remove them in favor of a revamped API.

--- rstem/tests/test_led_matrix.py
+++ rstem/tests/test_led_matrix.py
@@ -5,12 +5,18 @@ import rstem.led_matrix as led
 Automatic API tests for LED Matrix.
 '''
 
+def makefb(lines):
+    rows = []
+    for line in lines.splitlines():
+        if not line.strip():
+            continue
+        cols = [int(col, 16) for col in line.strip()]
+        rows += [cols]
+    rows = list(reversed(rows))
+    return rows
+
 def erased_fb(fb):
-    allzero = True
-    for row in fb:
-        for point in row:
-            if point != 0: allzero = False
-    return allzero
+    return makefb('00000000\n' * 8)
     
 def point_test(x, y, color):
     led.init_matrices()
@@ -31,39 +37,48 @@ def out_of_bounds_point_test(x, y):
     return erased_fb(led._framebuffer())
 
 @testing.automatic
-def init_matrices():
+def __old__init_matrices():
     led.init_matrices()
+    makefb('''
+        12345678
+        22223456
+        33334567
+        44445678
+        55556789
+        6666789a
+        777789ab
+        88889abc''')
     return True
 
 @testing.automatic
-def test_erased_fb():
+def __old__test_erased_fb():
     led.init_matrices()
     return erased_fb(led._framebuffer())
 
 @testing.automatic
-def point1():
+def __old__point1():
     return point_test(0, 0, 1)
 
 @testing.automatic
-def point2():
+def __old__point2():
     return point_test(1, 2, 3)
 
 @testing.automatic
-def point3():
+def __old__point3():
     return point_test(7, 7, 10)
 
 @testing.automatic
-def point_out_of_bounds1():
+def __old__point_out_of_bounds1():
     return out_of_bounds_point_test(-1, 0)
 
 @testing.automatic
-def point_out_of_bounds2():
+def __old__point_out_of_bounds2():
     return out_of_bounds_point_test(0, -1)
 
 @testing.automatic
-def point_out_of_bounds3():
+def __old__point_out_of_bounds3():
     return out_of_bounds_point_test(led.width, 0)
 
 @testing.automatic
-def point_out_of_bounds4():
+def __old__point_out_of_bounds4():
     return out_of_bounds_point_test(0, led.height)
