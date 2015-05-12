f854ede62a3a675fe02e801ab3531120a9245496
Brian Silverman <bri@raspberrystem.com>
2015-05-12 13:56:50 -0400

    install - Added target script to do install (apt-get/pip commands)

---
+++ scripts/tgtinstall.sh
@@ -0,0 +1,4 @@
+sudo apt-get update -y
+sudo apt-get install -y python3-pip libi2c-dev sox libsox-fmt-mp3 espeak chromium
+sudo pip-3.2 install raspberrystem raspberrystem-ide raspberrystem-projects
+
