7039bc5e454fc3f8099cc8170048dd5fb6acbc6e
Brian Silverman <bri@raspberrystem.com>
2015-06-09 16:40:58 -0400

    install - updated install script with latest required modules

--- scripts/tgtinstall.sh
+++ scripts/tgtinstall.sh
@@ -1,4 +1,6 @@
 sudo apt-get update -y
 sudo apt-get install -y python3-pip libi2c-dev sox libsox-fmt-mp3 espeak chromium
+sudo apt-get install -y libasound2-dev
+sudo pip-3.2 install pdoc
 sudo pip-3.2 install raspberrystem raspberrystem-ide raspberrystem-projects
 
