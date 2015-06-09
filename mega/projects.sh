cd8922402bfeab477499064e9020022ae92616a9
Brian Silverman <bri@raspberrystem.com>
2015-06-09 19:44:45 -0400

    mega - moved mega tests to their own directory

---
+++ mega/projects.sh
@@ -0,0 +1,12 @@
+echo "###################################################################"
+echo ""
+echo "Piano / Simon / Space Invaders Projects Test Lid"
+echo ""
+echo "Lid setup:"
+echo "  - Simon / Piano"
+echo "  - Space Invaders"
+echo ""
+echo "###################################################################"
+echo "Press Enter to continue..."
+read
+make test-projects
