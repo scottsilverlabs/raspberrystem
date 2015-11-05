a5fc8c972f691db847537db7e649332c30bcb4b9
Brian Silverman <bri@raspberrystem.com>
2015-11-05 08:14:14 -0500

    minecraft - added all move functions

--- rstem/mcpi/control.py
+++ rstem/mcpi/control.py
@@ -21,19 +21,81 @@ import os
 import time
 from subprocess import call, Popen, PIPE
 from functools import partial
+import uinput
+from threading import Timer
 
 shcall = partial(call, shell=True)
 shopen = partial(Popen, stdin=PIPE, stderr=PIPE, stdout=PIPE, close_fds=True, shell=True)
 
 MINECRAFT_WIN_NAME = 'Minecraft - Pi edition'
-XDOTOOL_CMD = "DISPLAY=:0 xdotool search --name '{:s}'".format(MINECRAFT_WIN_NAME)
+HIDE_WIN_CMD = "DISPLAY=:0 wmctrl -r '{:s}' -b add,hidden".format(MINECRAFT_WIN_NAME)
+SHOW_WIN_CMD = "DISPLAY=:0 wmctrl -a '{:s}'".format(MINECRAFT_WIN_NAME)
+
+keys = [
+    uinput.KEY_W,
+    uinput.KEY_A,
+    uinput.KEY_S,
+    uinput.KEY_D,
+    uinput.KEY_E,
+    uinput.KEY_1,
+    uinput.KEY_2,
+    uinput.KEY_3,
+    uinput.KEY_4,
+    uinput.KEY_5,
+    uinput.KEY_6,
+    uinput.KEY_7,
+    uinput.KEY_8,
+    uinput.KEY_SPACE,
+    uinput.KEY_ENTER,
+    uinput.KEY_LEFTSHIFT,
+    ]
+device = uinput.Device(keys)
+time.sleep(0.5)
 
 def show(show=True):
-    ret = shcall(XDOTOOL_CMD + " {:s} --sync".format("windowmap" if show else "windowunmap"))
+    ret = shcall(SHOW_WIN_CMD if show else HIDE_WIN_CMD)
     if ret:
         raise IOError('Could not show/hide minecraft window.  Is it running?')
 
 def hide():
     show(show=False)
 
-__all__ = ['show']
+def key_press(key, duration=None):
+    global device
+    device.emit(key, 1)
+    if duration != None:
+        Timer(duration,device.emit,args=[key, 0]).start()
+
+def backward(duration=None):
+    key_press(uinput.KEY_S, duration)
+
+def forward(duration=None):
+    key_press(uinput.KEY_W, duration)
+
+def left(duration=None):
+    key_press(uinput.KEY_A, duration)
+
+def right(duration=None):
+    key_press(uinput.KEY_D, duration)
+
+def jump(duration=None):
+    key_press(uinput.KEY_SPACE, duration)
+
+def crouch(duration=None):
+    key_press(uinput.KEY_LEFTSHIFT, duration)
+
+def ascend(duration=None):
+    jump(duration)
+
+def descend(duration=None):
+    crouch(duration)
+
+def stop(duration=None):
+    for key in keys:
+        device.emit(key, 0)
+        
+
+__all__ = [
+    'show',
+    'hide',
+    ]
--- rstem/tests/test_minecraft.py
+++ rstem/tests/test_minecraft.py
@@ -13,36 +13,53 @@ import time
 shcall = partial(call, shell=True)
 shopen = partial(Popen, stdin=PIPE, stderr=PIPE, stdout=PIPE, close_fds=True, shell=True)
 
-def start_minecraft():
+MINECRAFT_WIN_NAME = 'Minecraft - Pi edition'
+XDOTOOL_CMD = "DISPLAY=:0 xdotool search --name '{:s}'".format(MINECRAFT_WIN_NAME)
+HIDE_WIN_CMD = "DISPLAY=:0 wmctrl -r '{:s}' -b add,hidden".format(MINECRAFT_WIN_NAME)
+SHOW_WIN_CMD = "DISPLAY=:0 wmctrl -a '{:s}'".format(MINECRAFT_WIN_NAME)
+MINECRAFT_CMD = "DISPLAY=:0 /usr/bin/minecraft-pi >/dev/null &"
+KILL_MINECRAFT_CMD = "killall minecraft-pi 2>/dev/null"
+IS_RUNNING_MINECRAFT_CMD = "pgrep '^minecraft-pi$' >/dev/null"
+
+shcall(KILL_MINECRAFT_CMD)
+
+def start_minecraft(quit=True):
     def decorator(func):
         @wraps(func)
         def wrapper():
             # Setup
-            shcall(KILL_MINECRAFT_CMD)
-            shcall(MINECRAFT_CMD)
-            time.sleep(0.5)
+            if shcall(IS_RUNNING_MINECRAFT_CMD):
+                shcall(MINECRAFT_CMD)
+                time.sleep(0.5)
+                show(False)
 
             passed = func()
 
             # Teardown
-            shcall(KILL_MINECRAFT_CMD)
+            if quit:
+                shcall(KILL_MINECRAFT_CMD)
 
             return passed
         return wrapper
     return decorator
 
-def show_minecraft():
+def show_minecraft(hide=True):
     def decorator(func):
         @wraps(func)
         def wrapper():
             # Setup
-            show(True)
-            time.sleep(0.5)
+            if not window_visible(get_wid()):
+                show(True)
+                time.sleep(0.5)
+                shcall(XDOTOOL_CMD + " key Return")
+                shcall(XDOTOOL_CMD + " key Return")
+                time.sleep(2)
 
             passed = func()
 
             # Teardown
-            show(False)
+            if hide:
+                show(False)
 
             return passed
         return wrapper
@@ -57,15 +74,11 @@ def window_visible(wid):
         raise IOError('Cannot detect if window is mapped or not')
     return False
 
-XDOTOOL_CMD = "DISPLAY=:0 xdotool search --name 'Minecraft - Pi edition'"
-MINECRAFT_CMD = "DISPLAY=:0 /usr/bin/minecraft-pi &"
-KILL_MINECRAFT_CMD = "killall minecraft-pi 2>/dev/null"
-
 def get_wid():
     return int(shopen(XDOTOOL_CMD).stdout.read().strip())
 
 def show(show):
-    shcall(XDOTOOL_CMD + " {:s} --sync".format("windowmap" if show else "windowunmap"))
+    shcall(SHOW_WIN_CMD if show else HIDE_WIN_CMD)
 
 @testing.automatic
 def show_fails_if_minecraft_not_running():
@@ -126,4 +139,78 @@ def hide_closes():
         print('PASSED: Window should NOT be viewable, and it is not')
 
     return True
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_forward_then_stop():
+    '''Verify that the player moves forward for a half second'''
+    control.forward()
+    time.sleep(0.5)
+    control.stop()
+    time.sleep(0.5)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_backward_half_second():
+    '''Verify that the player does a 'backward' move for a half second'''
+    control.backward(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_forward_half_second():
+    '''Verify that the player does a 'forward' move for a half second'''
+    control.forward(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_jump_half_second():
+    '''Verify that the player does a 'jump' move for a half second'''
+    control.jump(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_crouch_half_second():
+    '''Verify that the player does a 'crouch' move for a half second'''
+    control.crouch(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_left_half_second():
+    '''Verify that the player does a 'left' move for a half second'''
+    control.left(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_right_half_second():
+    '''Verify that the player does a 'right' move for a half second'''
+    control.right(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_ascend_half_second():
+    '''Verify that the player does a 'ascend' move for a half second'''
+    control.ascend(duration=0.5)
+    time.sleep(1)
+
+@testing.manual_output
+@start_minecraft(quit=False)
+@show_minecraft(hide=False)
+def move_descend_half_second():
+    '''Verify that the player does a 'descend' move for a half second'''
+    control.descend(duration=0.5)
+    time.sleep(1)
 
