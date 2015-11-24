cd3014ac1a4b94741d27fd23c949a21db244af92
Brian Silverman <bri@raspberrystem.com>
2015-11-24 13:53:21 -0500

    minecraft - bridge and shovel projects

---
+++ projects/camp/minecraft/bridge2.py
@@ -0,0 +1,24 @@
+from rstem.accel import Accel
+from rstem.button import Button
+from rstem.mcpi import minecraft, control, block
+from rstem.mcpi.vec3 import Vec3
+import time
+from math import sin, cos, radians
+
+control.show(hide_at_exit=True)
+mc = minecraft.Minecraft.create()
+button = Button(7)
+
+while True:
+    if button.presses():
+        h = control.get_heading(mc)
+        x_delta = sin(radians(h))
+        z_delta = cos(radians(h))
+        pos = mc.player.getPos() - Vec3(0, 1, 0)
+        for i in range(30):
+            pos += Vec3(x_delta, 0, z_delta)
+            for width in range(-3,3+1):
+                mc.setBlock(pos + Vec3(width * z_delta, 0, width * x_delta), block.STONE)
+
+    time.sleep(0.01)
+
---
+++ projects/camp/minecraft/shovel2.py
@@ -0,0 +1,25 @@
+from rstem.accel import Accel
+from rstem.button import Button
+from rstem.mcpi import minecraft, control, block
+from rstem.mcpi.vec3 import Vec3
+import time
+from math import sin, cos, radians
+
+control.show(hide_at_exit=True)
+mc = minecraft.Minecraft.create()
+button = Button(7)
+
+def dig(mc, pos, radius=1):
+    r_vector = Vec3(radius,radius,radius)
+    mc.setBlocks(hit.pos - r_vector, hit.pos + r_vector, block.AIR)
+
+while True:
+    if button.presses():
+        control.hit()
+
+    hits = mc.events.pollBlockHits()
+    for hit in hits:
+        dig(mc, hit)
+
+    time.sleep(0.01)
+
