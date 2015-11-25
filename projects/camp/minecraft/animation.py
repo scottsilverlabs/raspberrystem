df75e06dfdb3fe733314b2f5a7e1458255ed1720
Brian Silverman <bri@raspberrystem.com>
2015-11-25 11:44:25 -0500

    minecraft - Animate a sprite on LED Matrix and Minecraft

---
+++ projects/camp/minecraft/animation.py
@@ -0,0 +1,69 @@
+from rstem.accel import Accel
+from rstem.button import Button
+from rstem.mcpi import minecraft, control, block
+from rstem.mcpi.vec3 import Vec3
+from rstem.led_matrix import FrameBuffer, Sprite
+from rstem.sound import Sound
+from itertools import cycle
+import time
+import sys
+from math import sin, cos, radians
+
+fb = FrameBuffer()
+
+control.show(hide_at_exit=True)
+mc = minecraft.Minecraft.create()
+
+animation = [
+    Sprite("""
+        00000000
+        0000FF00
+        000FF000
+        000FFF00
+        00FFFF00
+        000FFF00
+        0FF00F00
+        0F0000FF
+        """),
+    Sprite("""
+        00000000
+        0000FF00
+        0000F000
+        00FFFF00
+        0F0FF0F0
+        000FFF00
+        0FF00F00
+        00000FF0
+        """),
+    Sprite("""
+        00000000
+        0000FF00
+        000FF000
+        000FF000
+        000FF000
+        000FF000
+        00FFF000
+        000FF000
+        """),
+    Sprite("""
+        0000FF00
+        000FF000
+        000FFF00
+        00FFFF00
+        000FFF00
+        00F00F00
+        00F0F000
+        00F00000
+        """),
+    ]
+
+animation = cycle(animation)
+while True:
+    sprite = next(animation)
+
+    fb.erase()
+    fb.draw(sprite)
+    fb.show()
+
+    time.sleep(0.1)
+
